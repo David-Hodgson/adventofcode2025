8e83b88ec99956995e0ffbb5e25cfe36682d46b4
David Hodgson <dahodgson@gmail.com>
2025-12-01 13:35:39 +0000

    Day One - Part Two complete

--- dayone.cpp
+++ dayone.cpp
@@ -3,33 +3,33 @@
 #include <vector>
 using namespace std;
 
-int main(){
-	cout << "Day One\n";
-
+vector<string> readFile(string filename){
+	ifstream inputfile(filename);
 	string readLine;
-	int readCount = 0;
-	vector<string> commands;
-
-	ifstream inputFile("input_dayone.txt");
-
-	while (getline(inputFile, readLine)) {
-		commands.push_back(readLine);
-		readCount++;
+	vector<string> lines;
+	while(getline(inputfile,readLine)){
+					lines.push_back(readLine);
 	}
 
-	cout << "Read: " << readCount << " commands\n\n";
+	return lines;
+}
+
+void partOne(vector<string> commands){
+
+	cout << "Part One\n";
 
 	int currentPos = 50;
 	int zeroCount = 0;
 
+
 	for(int i=0;i<commands.size();i++){
-					cout << "Current Pos: " << currentPos << "\n";
-					cout << "Processing " << commands[i] << "\n";
+//					cout << "Current Pos: " << currentPos << "\n";
+//					cout << "Processing " << commands[i] << "\n";
 				 
 					char dir = commands[i][0];
 				 	int steps = std::stoi(commands[i].substr(1));
 
-					cout << "Dir: " << dir << ", Steps: " << steps << "\n";
+//					cout << "Dir: " << dir << ", Steps: " << steps << "\n";
 
 					if (dir == 'R') {
 									currentPos = currentPos + steps;
@@ -39,7 +39,7 @@ int main(){
 									currentPos = currentPos - steps;
 					}
 
-					cout << "New Pos (before adjustment): " << currentPos << "\n";
+//					cout << "New Pos (before adjustment): " << currentPos << "\n";
 					if (currentPos < 0){
 						while(currentPos<0){
 										currentPos = currentPos + 100;
@@ -55,4 +55,48 @@ int main(){
 	}
 
 	cout << "Number of zero's: " << zeroCount << "\n";
+}
+
+
+void partTwo(vector<string> commands){
+
+	cout << "Part Two\n";
+
+	int currentPos = 50;
+	int zeroCount = 0;
+
+	for(int i=0;i<commands.size();i++){
+				 
+					char dir = commands[i][0];
+				 	int steps = std::stoi(commands[i].substr(1));
+
+					cout << "Moving " << steps << " " << dir << "\n";
+
+					int stepDir = dir == 'R' ? 1 : -1;
+					for (int j=0;j<steps;j++)
+					{
+						currentPos = currentPos + stepDir;
+
+						if (currentPos > 99)
+										currentPos = 0;
+
+						if (currentPos < 0)
+										currentPos = 99;
+
+						if (currentPos ==0)
+										zeroCount++;
+									
+					}
+	}
+
+	cout << "Number of zero's: " << zeroCount << "\n";
+}
+
+int main(){
+
+	cout << "Day One\n";
+	vector<string> commands = readFile("input_dayone.txt");
+
+	partOne(commands);
+	partTwo(commands);
 }
